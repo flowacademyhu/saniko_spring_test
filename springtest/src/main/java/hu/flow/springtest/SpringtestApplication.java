84340ba0a2958d50f4ecd9edd77a5675e193b9cf
Aniko <s.aniko9424@gmail.com>
2021-02-08 10:43:04 +0100

    Initial

---
+++ springtest/src/main/java/hu/flow/springtest/SpringtestApplication.java
@@ -0,0 +1,13 @@
+package hu.flow.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestApplication.class, args);
+	}
+
+}
